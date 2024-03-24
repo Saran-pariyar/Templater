41d92c1963e9914802e3ca19faf72dab9b3fa4a6
Saran Pariyar <spariyar05@gmail.com>
2024-03-24 22:05:33 +0545

    Extra edit/ templater/ formatting

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-
-
-
-
-//replace the link with the API link here
 fetch('file:///E:/projects/integreate_api/data1.json')
-// fetch("E:/projects/integreate_api/data1.json")
+
     .then(response => response.json())
     .then(data => {
         const tableBody = document.getElementById('service_thing-table-body');
@@ -29,33 +24,25 @@ fetch('file:///E:/projects/integreate_api/data1.json')
     })
     .catch(error => console.error('Error fetching data:', error));
 
+const items = ["TPA", "TTN", "TYS", "XNA"]
+
+function populateDropdown(id, options) {
+    const dropdown = document.getElementById(id);
+
+    dropdown.innerHTML = '';
+
+    // Add new options
+    options.forEach(item => {
+        const option = document.createElement('option');
+        option.value = item;
+        option.text = item;
+        dropdown.add(option);
+    });
+}
 
 
-
-const items = [ "TPA", "TTN", "TYS", "XNA"]
-
-
-    function populateDropdown(id, options) {
-        const dropdown = document.getElementById(id);
-
-        dropdown.innerHTML = '';
-
-        // Add new options
-        options.forEach(item => {
-            const option = document.createElement('option');
-            option.value = item;
-            option.text = item;
-            dropdown.add(option);
-        });
-    }
-
-
-    populateDropdown('ori', item);
-
-
-    populateDropdown('dest', item);
-
-
+populateDropdown('ori', item);
+populateDropdown('dest', item);
 
 
 // GETTING DATA FROM FORM
@@ -66,23 +53,16 @@ document.getElementById('service_thingForm').addEventListener('submit', function
     const ori = document.getElementById('ori').value;
     const dest = document.getElementById('dest').value;
     let dd = document.getElementById('dd').value;
-    // Replace dashes with slashes in the date
     dd = dd.replace(/-/g, '/');
-  
-    
-    // edit url link with real one here
+
     const apiUrlForm = ``;
 
-// `http://ec2-16-171-37-194.eu-north-1.compute.amazonaws.com:8000/service_things?ori=DEN&dest=BNA&dd=2024-02-17`
-
-    // Fetch data from the API
     fetch(apiUrlForm)
-    // fetch("/data1.json")
+
         .then(response => response.json())
         .then(data => {
 
             const formTableBody = document.getElementById('form-service_thing-table-body');
-            // tableBody.innerHTML = '';
 
             data[0]?.forEach(service_thing => {
                 const row = document.createElement('tr');
